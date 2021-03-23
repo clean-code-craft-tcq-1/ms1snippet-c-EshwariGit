27231b96f66a024052fb946abbf6be58a33ad3c5
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-24 00:32:06 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,6 @@
 
 int _give_me_a_good_name(double value, double nextValue, double maxDelta);
 
-int validateSOCreadings(double* values,  int * numOfValues);
+int validateSOCreadings(double* values,  int * numOfValues1);
 
-int validateCurrentreadings(double* values, int * numOfValues1);
+int validateCurrentreadings(double* values, int * numOfValues2);
