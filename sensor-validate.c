c8c7fee8d5f237ef6ac24ba9a98857400b2d6803
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-24 00:24:01 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -9,9 +9,9 @@ int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
   return 1;
 }
 
-int validateSOCreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  lastButOneIndex;
+int validateSOCreadings(double* values, int * numOfValues) {
+  int * lastButOneIndex ;
+  
   for(int i = 0; i < lastButOneIndex; i++) {
     if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
       return 0;
@@ -20,9 +20,9 @@ int validateSOCreadings(double* values, int numOfValues) {
   return 1;
 }
 
-int validateCurrentreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  lastButOneIndex;
+int validateCurrentreadings(double* values, int * numOfValues) {
+  int* lastButOneIndex ;
+  
   for(int i = 0; i < lastButOneIndex; i++) {
     if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
       return 0;
