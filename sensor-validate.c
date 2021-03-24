72bc87c8852f41cc64c8af0b93dab9a2673b5a90
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-24 11:27:56 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,8 +1,9 @@
 #include "sensor-validate.h"
 
+int lastButOneIndex;
 int IndexCalculator( int numOfValues)
 {
-  int lastButOneIndex = numOfValues - 1;
+  lastButOneIndex = numOfValues - 1;
   return lastButOneIndex;
 }
 int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
@@ -13,7 +14,7 @@ int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
 }
 
 int validateSOCreadings(double* values, int numOfValues) {
-  IndexCalculator(int numOfValues);
+  IndexCalculator(numOfValues);
   for(int i = 0; i < lastButOneIndex; i++) {
     if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
       return 0;
@@ -23,7 +24,7 @@ int validateSOCreadings(double* values, int numOfValues) {
 }
 
 int validateCurrentreadings(double* values, int numOfValues) {
-  IndexCalculator(int numOfValues);
+  IndexCalculator(numOfValues);
   for(int i = 0; i < lastButOneIndex; i++) {
     if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
       return 0;
