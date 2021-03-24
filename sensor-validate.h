4f8c475868083c1be63cd2b1b6bd1a2eeaf045ef
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-24 19:17:02 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,6 @@
 
 int toleranceRangeValidation(double value, double nextValue, double maxDelta);
-
+int funcpointercalc(double* nextValue);
 int IndexCalculator( int numOfValues);
   
 int validateSOCreadings(double* values, int numOfValues);
