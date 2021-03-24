b521f848ffbdc5b021d58b160e5cab8a1cdeb083
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-24 19:46:17 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -18,6 +18,5 @@ TEST_CASE("reports error when current jumps abruptly") {
 // Testcase to check NULL function pointers.
 TEST_CASE("reports error when function pointers are NULL") {
   double currentReadings[] = {};
-  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(funcpointercalc(currentReadings, numOfCurReadings) == 0);
+    REQUIRE(funcpointercalc(currentReadings) == 0);
 }
