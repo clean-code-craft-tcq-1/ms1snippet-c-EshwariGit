9e1d06248fa0caf223c2c75d270d826b4ebcaab6
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-24 20:04:57 +0530

    Comments updated

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,7 +1,10 @@
 #include <cstddef>
 
+/* Function added to check NULL PTR*/
+int funcpointercalc(double* nextValue); 
+
 int toleranceRangeValidation(double value, double nextValue, double maxDelta);
-int funcpointercalc(double* nextValue);
+
 int IndexCalculator( int numOfValues);
   
 int validateSOCreadings(double* values, int numOfValues);
