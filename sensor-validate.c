c44e74124a09675a137409a55635d88cd001318d
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-24 18:24:06 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -6,7 +6,7 @@ int IndexCalculator( int numOfValues)
   lastButOneIndex = numOfValues - 1;
   return lastButOneIndex;
 }
-int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
+int toleranceRangeValidation(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return 0;
   }
@@ -16,7 +16,7 @@ int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
 int validateSOCreadings(double* values, int numOfValues) {
   IndexCalculator(numOfValues);
   for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
+    if(!toleranceRangeValidation(values[i], values[i + 1], 0.05)) {
       return 0;
     }
   }
@@ -26,7 +26,7 @@ int validateSOCreadings(double* values, int numOfValues) {
 int validateCurrentreadings(double* values, int numOfValues) {
   IndexCalculator(numOfValues);
   for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
+    if(!toleranceRangeValidation(values[i], values[i + 1], 0.1)) {
       return 0;
     }
   }
